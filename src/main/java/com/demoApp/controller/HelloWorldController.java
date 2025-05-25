f5f4fcf46da54f5e25094d08fff50b5b909a9c5a
srupesh0711 <sadirupeshr@gmail.com>
2025-05-25 12:27:13 +0530

    2nd commit

--- src/main/java/com/demoApp/controller/HelloWorldController.java
+++ src/main/java/com/demoApp/controller/HelloWorldController.java
@@ -1,5 +1,17 @@
 package com.demoApp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1")
 public class HelloWorldController {
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "hello rupesh";
+	}
 
 }
